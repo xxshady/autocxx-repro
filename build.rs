44c6ce54b6a1dba8b168cdc139a9abd6ffd8e3e2
xshady <fastik036@gmail.com>
2023-10-02 15:25:54 +0300

    ensure its broken

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
     autocxx_build::Builder::new("src/main.rs", [&"src"])
-        // .extra_clang_args(&["-std=c++20"])
+        .extra_clang_args(&["-std=c++20"])
         .build()
         .unwrap()
-        // .flag("/std:c++20")
+        .flag("/std:c++20")
         .compile("repro");
 }
